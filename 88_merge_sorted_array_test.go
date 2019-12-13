136fec8607acd9d25c4af372fead64b86f06bf61
Zhou Hua <zhouhua015@gmail.com>
2019-12-13 16:43:17 +0800

    Add problem #88: merge sorted array

---
+++ 88_merge_sorted_array_test.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+
+	for x := j; x >= 0; x-- {
+		nums1[k] = nums2[x]
+		k--
+	}
+}
